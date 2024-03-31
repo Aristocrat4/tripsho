727cd1a440e0cbd0644e5d9c316a4a0652d50b44
Aristocrat4 <galadanelia1@gmail.com>
2024-03-31 17:11:52 +0400

    add trust component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { OurserviceComponent } from './components/ourservice/ourservice.component';
+import { TrustComponent } from './components/trust/trust.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, OurserviceComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    OurserviceComponent,
+    TrustComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
